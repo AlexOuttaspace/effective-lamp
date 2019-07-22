08205ce5be3e1134abb0c434b2960a329350335b
AlexOuttaspace <matanforeveryone@gmail.com>
2019-07-22 03:57:25 +0300

    Updates

---
+++ precache-manifest.249fabee2a638b58e8ccb126c6357eea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9085849b2c9836cba30c25d71d54b34",
+    "url": "/effective-lamp/index.html"
+  },
+  {
+    "revision": "0ed22a99dad9e789ea28",
+    "url": "/effective-lamp/static/css/2.a5761821.chunk.css"
+  },
+  {
+    "revision": "46121500bd73a8f804ef",
+    "url": "/effective-lamp/static/css/main.9fffc6ea.chunk.css"
+  },
+  {
+    "revision": "0ed22a99dad9e789ea28",
+    "url": "/effective-lamp/static/js/2.1a1bc229.chunk.js"
+  },
+  {
+    "revision": "46121500bd73a8f804ef",
+    "url": "/effective-lamp/static/js/main.460c6588.chunk.js"
+  },
+  {
+    "revision": "8ded6e96c56826653a90",
+    "url": "/effective-lamp/static/js/runtime~main.702854eb.js"
+  }
+]);
